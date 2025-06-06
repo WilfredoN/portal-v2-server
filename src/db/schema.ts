8fc23727db7f3a4d324ee322d0fb22c4f910ecd9
WilfredoN <thekillermaps@gmail.com>
2025-06-06 18:41:23 +0300

    feat: sync of front\back schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -16,15 +16,13 @@ export const auth = pgTable('auth', {
   createdAt: timestamp('created_at', { withTimezone: true })
     .defaultNow()
     .notNull(),
-  signedInAt: timestamp('signed_in_at', { withTimezone: true }),
-  provider: varchar('provider', { length: 255 }).notNull(),
-  identifier: varchar('identifier', { length: 255 })
+  provider: varchar('provider', { length: 255 }).notNull()
 })
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
   email: varchar('email', { length: 255 }).notNull().unique(),
-  firstName: varchar('first_name', { length: 255 }).notNull(),
-  lastName: varchar('last_name', { length: 255 }).notNull(),
+  firstName: varchar('first_name', { length: 50 }).notNull(),
+  lastName: varchar('last_name', { length: 50 }).notNull(),
   status: userStatusEnum('status').notNull().default('new')
 })
