bc3958f439974867a441bcda2c02d424deacaff3
WilfredoN <thekillermaps@gmail.com>
2025-06-13 15:03:55 +0300

    feat: change-status test route

--- src/routes/test/test.route.ts
+++ src/routes/test/test.route.ts
@@ -1,5 +1,5 @@
 import { db } from '@src/db'
-import { roles } from '@src/db/schema'
+import { roles, users } from '@src/db/schema'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
 
@@ -58,6 +58,32 @@ testRoute.post('/remove-role', async content => {
     return content.json({ message: 'Role removed successfully' }, 200)
   } catch (error) {
     console.error('Error removing role:', error)
+    return content.json({ message: 'Internal Server Error' }, 500)
+  }
+})
+testRoute.post('/change-status', async content => {
+  try {
+    const { email, status } = await content.req.json()
+    if (!email || !status) {
+      return content.json({ message: 'Email and status is required' }, 400)
+    }
+
+    const response = await db
+      .update(users)
+      .set({ status: status })
+      .where(eq(users.email, email))
+      .returning()
+
+    if (response.length === 0) {
+      return content.json({ message: 'User not found' }, 404)
+    }
+
+    return content.json(
+      { message: 'User verified successfully', user: response[0] },
+      200
+    )
+  } catch (error) {
+    console.error('Error verifying user:', error)
     return content.json({ message: 'Internal Server Error' }, 500)
   }
 })
