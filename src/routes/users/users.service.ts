1c4675bb9d39b98fa4ad44746936c5b6ca1a68b6
WilfredoN <thekillermaps@gmail.com>
2025-06-15 21:18:12 +0300

    feat: encapsulate db related logic indo db/

--- src/routes/users/users.service.ts
+++ src/routes/users/users.service.ts
@@ -1,8 +1,8 @@
-import { db } from '@src/db'
+import { getAllUsers } from '@src/db/users'
 import { users } from '@src/db/schema'
 
 export const getUsers = async (): Promise<Array<typeof users.$inferSelect>> => {
-  const request = await db.select().from(users)
+  const request = await getAllUsers()
 
   if (request.length === 0) {
     throw new Error('No users found')
