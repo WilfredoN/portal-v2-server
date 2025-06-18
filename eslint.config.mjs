0dbe3510fe34769a2b26316e6078738d841594af
WilfredoN <thekillermaps@gmail.com>
2025-06-18 15:45:25 +0300

    fix: eslint confix fix regex

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,21 +3,20 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
     rules: {
         'antfu/no-top-level-await': 'off',
+        "node/no-process-env": 'off',
+        "node/prefer-global/process": 'off',
+        "no-console": ['warn', { allow: ['warn', 'error'] }],
         'perfectionist/sort-imports': ['error', {
             internalPattern: ['^@/'],
         }],
         'unicorn/filename-case': ['error', {
-            cases: {
-
-                kebabCase: true,
-                ignore: ['*.md'],
-            },
+            case: 'kebabCase',
+            ignore: ['.*\\.md$'],
         }],
-        'prefer-arrow-callback': 'error',
-        'antfu/top-level-functions': 'off',
-        'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
-        'style/brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
-        'ts/explicit-function-return-type': 'warn',
+        "antfu/top-level-function": 'off',
+        'prefer-arrow-callback': ['error'],
+        'antfu/top-level-function': ['off'],
+        'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
         'ts/consistent-type-imports': 'warn',
         'unused-imports/no-unused-imports': 'error',
         'unused-imports/no-unused-vars': [
