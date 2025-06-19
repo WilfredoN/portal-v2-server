4abb0ce0b14e04ac5b1d4ce9cc0947ac25b7ea0f
WilfredoN <thekillermaps@gmail.com>
2025-06-19 17:29:38 +0300

    feat: built-in jwt verification

--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -1,16 +1,16 @@
-import { authMiddleware } from '@src/middlewares/auth'
 import { requirePermission } from '@src/middlewares/role'
 import { PERMISSION, RESOURCES } from '@src/types/permissions'
 import { Hono } from 'hono'
+import { jwt } from 'hono/jwt'
 
 import { usersController } from './user.controller'
 
 export const usersRoute = new Hono()
 
-usersRoute.use('*', authMiddleware())
+usersRoute.use('*', jwt({ secret: process.env.JWT_SECRET! }))
 
 usersRoute.get(
   '/',
   requirePermission(RESOURCES.USERS, PERMISSION.VIEW),
-  usersController.getAll,
+  usersController.getAll
 )
